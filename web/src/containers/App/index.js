81e21ebef30b796ab0e87df850cef383a145dbf9
Nick Manna <manna422@gmail.com>
2018-05-03 00:06:37 -0400

    React Router setup

---
+++ web/src/containers/App/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from '../Home';
+import NotFound from '../../components/NotFound';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Route exactly pattern="/" component={Home} />
+          <Switch component={NotFound} />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
